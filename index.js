7cdbd77877ba6e0d203f9ccaa23c0764949e6a9e
kkrackerone <kkrackerone@yahoo.com>
2024-10-26 11:26:24 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const greeting = document.getElementById("greeting")
+greeting.style.textAlign = "center"
+greeting.style.marginTop = "50px"
+const username = window.prompt("What's your name?", "The Cookie Monster")
